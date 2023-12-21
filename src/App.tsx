255316a5720bf320378b7ba9cd9e2355710039de
Valentina Valverde <vvgreenville@gmail.com>
2023-12-21 14:26:12 -0500

    Props and prop types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <div>App</div>;
+//FC is a type to create a component
+import { FC } from "react";
+
+//you can use "interface" instead of "type" to declare component types
+//this defines the type of the prop
+type AppProps = {
+  title: string;
+};
+
+const App: FC<AppProps> = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
 };
 
 export default App;
